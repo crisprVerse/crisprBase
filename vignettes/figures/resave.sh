3080f386ffbc08b075ef00f590eeb44ccf352f61
fortinj2 <fortin.jeanphilippe@gene.com>
2022-01-04 18:07:08 -0800

    Added cartoon to vignette for BE

--- vignettes/figures/resave.sh
+++ vignettes/figures/resave.sh
@@ -1,4 +1,5 @@
 #Required inkscape to be installed
 ink enzymes.pdf --export-plain-svg=enzymes.svg
 ink nucleases.pdf --export-plain-svg=nucleases.svg
+ink nucleasesBaseEditor.pdf --export-plain-svg=nucleasesBaseEditor.svg
 ink cut_sites.pdf --export-plain-svg=cut_sites.svg
